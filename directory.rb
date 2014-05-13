66bbbefd4bee912b78f8739a35b41f6a2c8b7ded
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-05-13 11:54:46 +0100

    asked for input of student names

--- directory.rb
+++ directory.rb
@@ -1,29 +1,33 @@
-puts "The students of my cohort at Makers Academy"
-puts "----------"
-students = ["Jamie Patel",
-"Faezrah Rizalman",
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"The Alien",
-"Terminator",
-"Freddy Kruger",
-"The Joker"]
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
 	puts "---------------"
 end
 
-def print(names)
-names.each { |name| puts name}
+def print(students)
+students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end
 
 def print_footer(names)
 	puts "Overall we have #{names.length} great students"
 end
 
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	students = []
+	name = gets.chomp
+
+	while !name.empty? {
+		students << {:name => name, :cohort => :May}
+		puts "Now we have #{students.length} students"
+		name = gets.chomp
+	}
+	return students
+end
+
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
