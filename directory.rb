db74bd93c66482dae1821770c469199222df51ed
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-05-14 16:04:30 +0100

    updated load

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+@students = []
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -5,23 +6,24 @@ def print_header
 end
 
 def print_students_list
-@students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+@students.each do |student|
+	puts "#{student[:name]} (#{student[:cohort]} cohort)"
+end
 end
 
 def print_footer
 	puts "Overall we have #{@students.length} great students"
 end
 
-
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish, just hit return twice"
 	@students = []
-	name = gets.chomp
+	name = STDIN.gets.chomp
 	while !name.empty? do
-		@students << {:name => name, :cohort => :May}
+		add_student(name, :May)
 		puts "Now we have #{@students.length} students"
-		name = gets.chomp
+		name = STDIN.gets.chomp
 	end
 	return @students
 end
@@ -52,14 +54,31 @@ def save_students
 	puts "Data saved!"
 end
 
-def load_students
-	file = File.open("students.csv", "r")
+def add_student(name, cohort)
+  @students << {:name => name, :cohort => cohort.to_sym}
+end
+
+def load_students(filename="students.csv")
+	file = File.open(filename, "r")
 	file.readlines.each do |line|
 		name, cohort = line.chomp.split(",")
-		@students << {name: name, cohort: cohort.to_sym}
+		add_student(name, cohort)
 	end
 	file.close
 end
+
+def try_load_students
+	filename = ARGV.first
+	return if filename.nil?
+	if File.exists?(filename)
+		load_students(filename)
+		puts "Loaded #{@students.length} from #{filename}"
+	else
+		puts "Sorry, #{filename} doesn't exist"
+		exit
+	end
+end
+
 
 def process(selection)
 	case selection
@@ -70,7 +89,7 @@ def process(selection)
 	when "3"
 		save_students
 	when "4"
-		load_students
+		try_load_students
 	when "9" 
 		exit
 	else puts "I don't know what you mean, try again"
@@ -80,8 +99,7 @@ end
 def interactive_menu
 	loop do
 	print_menu
-	selection = gets.chomp
-	process(selection)
+	process(STDIN.gets.chomp)
 end
 end
 
